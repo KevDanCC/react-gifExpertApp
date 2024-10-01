5abaf0fe12f5a96fb962a375c289636622eff488
Kevin Cisneros <kev.cisn@hotmail.com>
2024-09-30 19:47:34 -0600

    Seccion 11. Profundizando Hooks - useContext

---
+++ src/09-useContext/LoginPage.jsx
@@ -0,0 +1,31 @@
+
+import { useContext } from 'react';
+import { UserContext } from './context/UserContext'
+
+export const LoginPage = () => {
+
+    const { user, setUser } = useContext(UserContext);
+    return (
+        <>
+
+            <h1>LoginPage</h1>
+            <hr />
+
+            <pre>
+                {JSON.stringify(user, null, 3)}
+            </pre>
+
+            <button
+                className="btn btn-primary"
+                onClick={() => setUser({
+                    id: 123,
+                    name: 'KCisneros',
+                    email: "test@test.com"
+                })}
+            >
+                Estabelcer User
+            </button>
+
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -14,9 +14,13 @@ import { CallbackHook } from './07-callbackHook/CallbackHook.jsx'
 import { Padre } from './07-tarea-memo/Padre.jsx'
 import './08-useReducer/intro-reducer.js'
 import { TodoApp } from './08-useReducer/TodoApp.jsx'
+import { MainApp } from './09-useContext/MainApp.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <TodoApp />
-    </StrictMode>,
+    <BrowserRouter>
+        <StrictMode>
+            <MainApp />
+        </StrictMode>
+    </BrowserRouter>
 )
